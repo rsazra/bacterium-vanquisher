a4beba22d52da4f04d9459719afe177b1cf3f3b6
rsazra <rajbir.singh.azra@gmail.com>
2025-04-23 22:33:52 -0400

    Initial Commit

---
+++ swift-bv-game/swift_bv_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_bv_gameApp.swift
+//  swift-bv-game
+//
+//  Created by Rajbir Singh Azra on 2025-04-23.
+//
+
+import SwiftUI
+
+@main
+struct swift_bv_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
